43c2494cf619f3649baa78dd0e29df0682a9ad2e
Rahhal Rizqy <rarahhal@e1r2p1.1337.ma>
2022-04-19 21:53:25 +0000

    finish

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 16:06:05 by rarahhal          #+#    #+#             */
-/*   Updated: 2022/04/19 21:22:44 by rarahhal         ###   ########.fr       */
+/*   Updated: 2022/04/19 21:49:37 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,7 @@ int	main(int argc, char *argv[], char **envp)
 	bonus.cmd_paths = ft_split(bonus.paths, ':');
 	bonus.indx = -1;
 	while (++bonus.indx < bonus.cmd_nbr)
-	{
 		child(bonus, argv, envp);
-	}
 	close_pipes(&bonus);
 	while (bonus.indx-- > 0)
 		waitpid(-1, NULL, 0);
